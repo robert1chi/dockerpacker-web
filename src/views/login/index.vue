0d53d7208e5008d63b76108689b6539a166f6893
robert1chi <40431036+robert1chi@users.noreply.github.com>
2022-08-04 14:32:16 +0800

    to: login page

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,26 @@
 <script lang="ts" setup>
-import { NButton } from "naive-ui";
+import { NButton, NForm, FormInst, useMessage } from "naive-ui";
+import store from "@/store/index.js";
+
+if(localStorage.getItem("session-token")) {
+  localStorage.removeItem("session-token");
+} 
+
+const loginSubmit =()=>{
+  console.log("loginSubmit"); 
+}
 </script>
 <template>
   <div>
-    <NButton>Hello</NButton>
+    <n-form>
+      <n-form-item>
+        <n-input></n-input>
+      </n-form-item>
+      <n-form-item>
+        <n-input></n-input>
+      </n-form-item>
+    </n-form>
+    <n-button type="primary" @click="loginSubmit
+    ">Hello</n-button>
   </div>
 </template>
