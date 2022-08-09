1c4a0f161f1a3eb4a3614d33b2a6a9a85317728a
robert1chi <40431036+robert1chi@users.noreply.github.com>
2022-08-09 16:03:49 +0800

    to: layout component

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,6 +1,8 @@
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import { NLayout, NLayoutHeader, NLayoutContent, NLayoutFooter } from "naive-ui";
+import { NLayout, NLayoutHeader, NLayoutContent, NLayoutFooter, NSpace, NButton } from "naive-ui";
+
+// import './index.css'
 
 const name = `dockerpacker-base-layout`;
 
@@ -9,17 +11,64 @@ export default defineComponent({
     setup() {
         const router = useRouter();
         const route = useRoute();
+        const contentHeight = ref('height: 80vh');
+        onMounted(() => {
 
+        })
+        const list = ref([
+            { label: '首页', path: '/' },
+            { label: '项目', path: '/project' },
+        ]);
+
+        const navibar = () => {
+            return (
+                <div className="header-container flex justify-between items-center">
+                    <div className="header-title flex items-center">
+                        <span>Docker Packer</span>
+                    </div>
+                    <div className="header-menu space-x-6 flex">
+                        {
+                            list.value.map(item => {
+                                return (
+                                    <div className="header-menu-item">
+                                        <NButton quaternary size="small" onClick={() => {
+                                            router.push(item.path);
+                                        }}>{item.label}</NButton>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            )
+        }
+
+        return {
+            navibar,
+            contentHeight,
+        }
     },
     render() {
+        const Navibar = this.navibar
+
         return (
             <div>
-                <NLayout className="h-max">
-                    <NLayoutHeader className="h-4">颐和园路</NLayoutHeader>
-                    <NLayoutContent>
-                        <router-view />
-                    </NLayoutContent>
-                    <NLayoutFooter>成府路</NLayoutFooter>
+                <NLayout className="n-layout n-layout--static-positioned h-screen">
+                    <div className="p-4 h-16 bg-[rgba(128,128,128,0.2)] content-center flex items-center">
+                        <NLayoutHeader style={"background: rgba(0,0,0,0);"}>
+                            <Navibar />
+                        </NLayoutHeader>
+                    </div>
+                    <div className="min-h-[calc(100vh-8rem)] p-4">
+                        <NLayoutContent>
+                            <NSpace vertical size="large">
+                                <router-view />
+                            </NSpace>
+                        </NLayoutContent>
+                    </div>
+                    <div className="p-4 min-h-[4rem] bg-[rgba(128,128,128,0.2)]">
+                        <NLayoutFooter style={"background: rgba(0,0,0,0);"}>成府路</NLayoutFooter>
+                    </div>
                 </NLayout>
             </div>
         );
