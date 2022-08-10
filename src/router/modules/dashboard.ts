a6b8360848c33656a8e29c79a3d3c43f4a820457
robert1chi <40431036+robert1chi@users.noreply.github.com>
2022-08-10 11:35:41 +0800

    to (router): add routers

---
+++ src/router/modules/dashboard.ts
@@ -0,0 +1,19 @@
+import Layout from '@/layouts'
+
+export default [
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: Layout,
+        redirect: '/dashboard/index',
+        meta: { title: 'dashboard.title' },
+        children: [{
+            path: 'index',
+            name: 'index',
+            component: () => import('@/views/dashboard'),
+            meta: {
+                title: 'dashboard.index'
+            }
+        }]
+    }
+]
