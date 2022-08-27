790f138c2c7101c0a850f94e6ff094df5792b570
robert1chi <40431036+robert1chi@users.noreply.github.com>
2022-08-27 10:26:19 +0800

    to (interface): add api interfaces

--- src/utils/api/system.ts
+++ src/utils/api/system.ts
@@ -1,26 +1,21 @@
 import request from '@/utils/request'
+import { ApiResponse } from './api.global'
 
-declare namespace Login {
-    type Input = {
-        username: string,
-        password: string
-    }
-    type Output = {
-        code: number,
-        data: {
-            token: string,
-            userInfo: {
-                roles: string[]
-            }
-        }
-        msg: string
+type LoginInput = {
+    username: string,
+    password: string
+}
+type LoginOutput = {
+    token: any
+    userInfo: {
+        roles: string[]
     }
 }
 
 /**
  * Login Controller
  */
-export const systemLogin = (data: Login.Input): Promise<Login.Output> => {
-    return request.post('/api/login', data)
+export const systemLogin = (data: LoginInput): ApiResponse<LoginOutput> => {
+    return request.post('/users/login', data)
 }
 
